b6fc00237bdef872f874bad96e553c80996a331d
Ssadaf <s.sadaf2277@gmail.com>
2017-08-12 10:52:48 -0700

    student class added

--- Wizardingworld/src/entities/Student.java
+++ Wizardingworld/src/entities/Student.java
@@ -1,5 +1,35 @@
 package entities;
 
-public class Student {
+import java.util.*;
 
+public class Student extends Person {
+	private Vector <Course> courses;
+	private Vector <FinishedCourse> reportCard;
+	
+	//setters and getters
+	public Vector<Course> getCourses() {
+		return courses;
+	}
+	public void setCourses(Vector<Course> _courses) {
+		courses = _courses;
+	}
+	public Vector<FinishedCourse> getReportCard() {
+		return reportCard;
+	}
+	public void setReportCard(Vector<FinishedCourse> _reportCard) {
+		reportCard = _reportCard;
+	}
+	
+	//constructor
+	public Student(String _name , String _houseName , String _bloodStatus , String _school , Date _birthday , Vector <Course> _courses , Vector <FinishedCourse> _reportCard){
+		super(_name ,_houseName , _bloodStatus , _school , _birthday);
+		courses = _courses;
+		reportCard = _reportCard;
+	}
+	public Student(String _name, Vector <Course> _courses , Vector <FinishedCourse> _reportCard){
+		this(_name , "" , "" , "" ,null ,_courses , _reportCard);
+	}
+	public Student(){
+		this("" , "" , "" , "" ,null , null , null);
+	}
 }
